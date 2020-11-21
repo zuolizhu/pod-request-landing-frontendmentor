ed8965f573a5d8987b4190e819a112ff1317ce26
zuolizhu <zuolizhu5960x@gmail.com>
2020-11-21 13:01:27 -0500

    mobile style finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.scss';
 import PageTitle from './components/PageTitle';
+import logo from './assets/desktop/logo.svg';
+import Platforms from './components/Platforms';
+import RequestForm from './components/RequestForm';
 
 function App() {
   return (
     <div className="App">
-      <PageTitle />
-      <p>
-      Publish your podcasts everywhere.
+      <div className="logo">
+        <img src={logo} alt="site logo"/>
+      </div>
+      <div className="content-container">
+        <PageTitle />
+        <p className="t-body page-body">Upload your audio to Pod with a single click. We’ll then distribute your podcast to Spotify, Apple Podcasts, Google Podcasts, Pocket Casts and more!</p>
+        <Platforms />
+        <RequestForm />
+      </div>
 
-      Upload your audio to Pod with a single click. We’ll then distribute your podcast to Spotify, 
-      Apple Podcasts, Google Podcasts, Pocket Casts and more!
+      <p>
 
       Request access
       </p>
--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function PageTitle() {
   return (
-    <h1 className="t-title page-title"><span className="t-cyan">Publish your podcasts</span><span className="t-white">everywhere.</span></h1>
+    <h1 className="t-title page-title"><span className="t-cyan">Publish your podcasts</span><br/><span className="t-white">everywhere.</span></h1>
   )
 }
