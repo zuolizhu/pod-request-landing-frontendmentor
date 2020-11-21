dc024953b7889b9898a92db5e54c081624f896a0
zuolizhu <zuolizhu5960x@gmail.com>
2020-11-21 11:32:42 -0500

    project structure setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import PageTitle from './components/PageTitle';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PageTitle />
+      <p>
+      Publish your podcasts everywhere.
+
+      Upload your audio to Pod with a single click. We’ll then distribute your podcast to Spotify, 
+      Apple Podcasts, Google Podcasts, Pocket Casts and more!
+
+      Request access
+      </p>
     </div>
   );
 }
---
+++ src/components/PageTitle.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PageTitle() {
+  return (
+    <h1 className="t-title page-title"><span className="t-cyan">Publish your podcasts</span><span className="t-white">everywhere.</span></h1>
+  )
+}
