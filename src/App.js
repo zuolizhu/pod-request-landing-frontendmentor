9e33bc4ed3295227ced368ac3a6a129a3d89db19
zuolizhu <zuolizhu5960x@gmail.com>
2020-11-22 13:10:46 -0500

    bring animation back

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import { useRef } from 'react';
-// import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import './App.scss';
 import PageTitle from './components/PageTitle';
 import logo from './assets/desktop/logo.svg';
 import Platforms from './components/Platforms';
 import RequestForm from './components/RequestForm';
-// import gsap from 'gsap';
+import gsap from 'gsap';
 
 function App() {
 
@@ -16,15 +15,15 @@ function App() {
   const pageTitleRef = useRef(null);
 
 
-  // useEffect(() => {
-  //   const timeline = gsap.timeline();
-  //   timeline.fromTo(logoRef.current, { opacity: 0, y: '50%' }, { opacity: 1, y: '0%', duration: 0.5 });
-  //   timeline.fromTo(pageTitleRef.current, { opacity: 0, y: '50%' }, { opacity: 1, y: '0%', duration: 1 });
-  //   timeline.fromTo(pageBodyRef.current, { opacity: 0, x: '15%' }, { opacity: 1, x: '0%', duration: 1 }, "-=0.5");
-  //   timeline.fromTo(formRef.current, { opacity: 0, x: '-15%' }, { opacity: 1, x: '0%', duration: 1 }, "-=0.5");
-  //   timeline.fromTo(platformRef.current, { opacity: 0, x: '5%' }, { opacity: 1, x: '0%', duration: 1 }, "-=0.5");
+  useEffect(() => {
+    const timeline = gsap.timeline();
+    timeline.fromTo(logoRef.current, { opacity: 0, y: '50%' }, { opacity: 1, y: '0%', duration: 0.5 });
+    timeline.fromTo(pageTitleRef.current, { opacity: 0, y: '50%' }, { opacity: 1, y: '0%', duration: 1 });
+    timeline.fromTo(pageBodyRef.current, { opacity: 0, x: '15%' }, { opacity: 1, x: '0%', duration: 1 }, "-=0.5");
+    timeline.fromTo(formRef.current, { opacity: 0, x: '-15%' }, { opacity: 1, x: '0%', duration: 1 }, "-=0.5");
+    timeline.fromTo(platformRef.current, { opacity: 0, x: '5%' }, { opacity: 1, x: '0%', duration: 1 }, "-=0.5");
 
-  // }, []);
+  }, []);
   return (
     <div className="App">
       <div ref={logoRef} className="logo">
